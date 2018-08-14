65c69c188a9035a3b4ab6c730d0030c2997c94f7
Everardo Vazquez <ever.vazquezt@gmail.com>
2018-08-14 16:30:51 -0500

    Cambios Caja

--- controllers/CajaController.php
+++ controllers/CajaController.php
@@ -37,7 +37,6 @@ class CajaController extends Controller
      */
     public function actionIndex()
     {
-
         $searchModel = new CajaSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         $estado_caja = new EstadoCaja();
@@ -70,24 +69,26 @@ class CajaController extends Controller
     public function actionCreate()
     {
         $model = new Caja();
-        $model->create_user=Yii::$app->user->identity->id;
-
-        if ($model->load(Yii::$app->request->post())) {
-
+        if ($model->load(Yii::$app->request->post())) 
+        {
           $model->create_user=Yii::$app->user->identity->id;
           $model->create_time=date('Y-m-d H:i:s');
-
-          if($model->tipo_movimiento = 1){
-            $model->efectivo=-($model->efectivo);
-          } else {
+          if($model->tipo_movimiento === 1)
+            $model->efectivo=-($model->efectivo); 
+          else 
             $model->efectivo= $model->efectivo;
+          if($model->save())
+          {
+                $searchModel = new CajaSearch();
+                $estado_caja = Yii::$app->db->createCommand('SELECT * FROM estado_caja WHERE id = 1')->queryAll();
+                $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+                return $this->render('index', [
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
+                    'estado_caja' => $estado_caja,
+                ]);
           }
-            if($model->save()){
-              return $this->redirect(['index', 'id' => $model->id]);
-            }
-
         }
-
         return $this->renderAjax('create', [
             'model' => $model,
         ]);
@@ -101,48 +102,35 @@ class CajaController extends Controller
     public function actionApertura()
     {
         $model = new Caja();
-
         $totalCaja=Yii::$app->db->createCommand('SELECT Sum(efectivo), Sum(tarjeta), Sum(deposito) FROM caja AS Caja')->queryAll();
-        $estado_caja = Yii::$app->db->createCommand('SELECT * FROM estado_caja WHERE id = 1')->queryAll();
-
-        $sql = EstadoCaja::findOne(['id' => 1]);
-        $sql->estado_caja = 1;
-        $sql->save();
-
         if ($model->load(Yii::$app->request->post()))
         {
             $model->descripcion="Apertura de caja";
             $model->tipo_movimiento = 0;
             $model->create_user=Yii::$app->user->identity->id;
             $model->create_time=date('Y-m-d H:i:s');
-            if($model->save())
-            //if ($model->save()&&$estadoCaja->save())
+            $sql = EstadoCaja::findOne(['id' => 1]);
+            $sql->estado_caja = 1;
+            if($model->save() && $sql->save())
             {
-                $searchModel = new CajaSearch();
-                $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
+                $searchModel = new CajaSearch();
+                $estado_caja = new EstadoCaja();
+                $estado_caja = Yii::$app->db->createCommand('SELECT * FROM estado_caja WHERE id = 1')->queryAll();
+                $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
                 return $this->render('index', [
                     'searchModel' => $searchModel,
                     'dataProvider' => $dataProvider,
                     'estado_caja' => $estado_caja,
                 ]);
             }
-
         }
-
         return $this->renderAjax('apertura', [
             'model' => $model,
             'totalCaja'=>$totalCaja,
         ]);
     }
 
-    /*
-      ID => folio
-      FECHA,
-      DESCRIPCIÓN,
-      TIPO MOVIMIENTO Y
-      CREATE USER
-    */
 
     /**
      * Cierre de Caja.
@@ -153,66 +141,44 @@ class CajaController extends Controller
     {
         $model = new Caja();
         $estado_caja = new EstadoCaja();
-
         $totalCaja = Yii::$app->db->createCommand('SELECT Sum(efectivo), Sum(tarjeta), Sum(deposito) FROM caja AS Caja')->queryAll();
-        $totalesRetirado = Yii::$app->db->createCommand('SELECT * FROM caja WHERE id=(SELECT MAX(id) FROM caja WHERE descripcion=\'Cierre de caja\')')->queryAll();
-        $habitacionesRealizadas = Yii::$app->db->createCommand('SELECT * FROM reservacion WHERE create_time BETWEEN (SELECT MAX(create_time) FROM caja WHERE descripcion=\'Apertura de caja\') AND (SELECT MAX(create_time) FROM caja WHERE descripcion=\'Cierre de caja\')')->queryAll();
-        $estado_caja = Yii::$app->db->createCommand('SELECT * FROM estado_caja WHERE id = 1')->queryAll();
-
-        $sql = EstadoCaja::findOne(['id' => 1]);
-        $sql->estado_caja = 0;
-        $sql->save();
-
-        $numHabitaciones = Count($habitacionesRealizadas);
-
         if ($model->load(Yii::$app->request->post()))
         {
+            $sql = EstadoCaja::findOne(['id' => 1]);
+            $sql->estado_caja = 0;
             $model->descripcion="Cierre de caja";
             $model->tipo_movimiento=1;
             $model->efectivo=-($model->efectivo);
             $model->create_user=Yii::$app->user->identity->id;
             $model->create_time=date('Y-m-d H:i:s');
-
-           if($model->save())
-            // if ($model->save()&&$estadoCaja->save())
+            if($model->save() && $sql->save())
             {
+                $totalesRetirado = Yii::$app->db->createCommand('SELECT * FROM caja WHERE id=(SELECT MAX(id) FROM caja WHERE descripcion=\'Cierre de caja\')')->queryAll();
+                $habitacionesRealizadas = Yii::$app->db->createCommand('SELECT COUNT(*) FROM reservacion WHERE create_time BETWEEN (SELECT MAX(create_time) FROM caja WHERE descripcion=\'Apertura de caja\') AND (SELECT MAX(create_time) FROM caja WHERE descripcion=\'Cierre de caja\')')->queryAll();
                 $searchModel = new CajaSearch();
                 $dataProvider = $searchModel->buscarMovimientosCierre(Yii::$app->request->queryParams);
+                $content = $this->renderPartial('corteCaja',[
+                    'dataProvider' => $dataProvider,
+                    'totalesRetirados'=>$totalesRetirado,
+                    'totalCaja'=>$totalCaja,
+                    'numHabitaciones'=>$habitacionesRealizadas
+                ]);
 
-              $content = $this->renderPartial('corteCaja',[
-
-                'dataProvider' => $dataProvider,
-                'searchModel' => $searchModel,
-                'totalesRetirados'=>$totalesRetirado,
-                'totalCaja'=>$totalCaja,
-                'numHabitaciones'=>$numHabitaciones
-              ]);
-                // setup kartik\mpdf\Pdf component
-              $pdf = new Pdf([
-                  // set to use core fonts only
-                  'mode' => Pdf::MODE_CORE,
-                  // A4 paper format
-                  'format' => Pdf::FORMAT_A4,
-                  // portrait orientation
-                  'orientation' => Pdf::ORIENT_PORTRAIT,
-                  // stream to browser inline
-                  'destination' => Pdf::DEST_BROWSER,
-                  // your html content input
-                  'content' => $content,
-                  // format content from your own css file if needed or use the
-                  // enhanced bootstrap css built by Krajee for mPDF formatting
-                  'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
-                  // any css to be embedded if required
-                  'cssInline' => '.kv-heading-1{font-size:18px}',
-                   // set mPDF properties on the fly
-                  'options' => ['title' => 'Reporte Cierre'],
-                   // call mPDF methods on the fly
-                  'methods' => [
-                      'SetHeader'=>['Reporte de cierre de caja '. date('d-m-Y')],
-                      'SetFooter'=>['Página {PAGENO}'],
-                  ]
-              ]);
-              return $pdf->render();
+                $pdf = new Pdf([
+                    'mode' => Pdf::MODE_CORE,
+                    'format' => Pdf::FORMAT_A4,
+                    'orientation' => Pdf::ORIENT_PORTRAIT,
+                    'destination' => Pdf::DEST_BROWSER,
+                    'content' => $content,
+                    'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
+                    'cssInline' => '.kv-heading-1{font-size:18px}',
+                    'options' => ['title' => 'Reporte Cierre'],
+                    'methods' => [
+                        'SetHeader'=>['Reporte de cierre de caja '. date('d-m-Y')],
+                        'SetFooter'=>['Página {PAGENO}'],
+                    ]
+                ]);
+                return $pdf->render();
             }
         }
 
@@ -223,25 +189,7 @@ class CajaController extends Controller
         ]);
     }
 
-    /**
-     * Updates an existing Caja model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-        $model->create_user=Yii::$app->user->identity->id;
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
 
     /**
      * Deletes an existing Caja model.
